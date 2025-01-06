151b59e633112876352969eceec6082135d441b3
Suraj Pathak <suraj.pathak@axzorait.com>
2025-01-06 23:54:38 +0530

    virtualized list to render big lists in chunks

--- frontend/src/components/VirtualizedList.jsx
+++ frontend/src/components/VirtualizedList.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
-export const VirtualizedList = () => {
+export const VirtualizedList = ({ data }) => {
+  const [dataInChunks, setDataInChunks] = useState([]);
+  const chunkSize = 10;
+  const listRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const chuncked = data.slice(0, chunkSize);
+    setDataInChunks(chuncked);
+  }, [data]);
+
+  const handleScroll = () => {
+    if (!listRef.current) return;
+    const bottom =
+      listRef.current.scrollHeight ===
+      listRef.current.scrollTop + listRef.current.clientHeight;
+    if (bottom) {
+      setIsLoading(true);
+      const chuncked = data.slice(
+        dataInChunks.length,
+        chunkSize + dataInChunks.length
+      );
+
+      setDataInChunks((prev) => [...prev, ...chuncked]);
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div>VirtualizedList</div>
-  )
-}
+    <div
+      ref={listRef}
+      style={{
+        border: "1px solid",
+        width: "400px",
+        height: "200px",
+        overflow: "scroll",
+      }}
+      onScroll={handleScroll}
+    >
+      {isLoading && <div>Loading...</div>}
+      {!isLoading &&
+        dataInChunks?.map((item, index) => <div key={index}>{item.name}</div>)}
+    </div>
+  );
+};
--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { VirtualizedList } from '../components/VirtualizedList'
+import React from "react";
+import { VirtualizedList } from "../components/VirtualizedList";
 
 export const List = () => {
+  const data = Array.from(
+    {
+      length: 1000,
+    },
+    (_, i) => ({
+      id: i,
+      name: `Item ${i + 1}`,
+      description: `Description for Item ${i + 1}`,
+    })
+  );
+
   return (
     <div>
-        {/* <VirtualizedList/> */}
+      <h1>List</h1>
+      <VirtualizedList data={data} />
     </div>
-  )
-}
+  );
+};
